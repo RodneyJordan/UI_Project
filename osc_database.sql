45b1afa95e319d0ea988caaaff9eda5d7e0dd7a6
Rodney Jordan <rodneycjordan@gmail.com>
2015-10-08 20:04:27 -0500

    edited the database file

--- osc_database.sql
+++ osc_database.sql
@@ -1,8 +1,17 @@
 CREATE TABLE IF NOT EXISTS USER (
+   UID int(8) NOT NULL, AUTO_INCREMENT,
    Fname varchar(15),
    Minit char(1),
    Lname varchar(15),
    UserName varchar(50) NOT NULL UNIQUE,
    Password varchar(15) NOT NULL UNIQUE,
-   PRIMARY KEY (UserName),
+   PRIMARY KEY (UID),
 ) ENGINE = MyISAM;
+
+CREATE TABLE IF NOT EXISTS MESSAGE (
+   
+   UID int(8) NOT NULL,
+   Msg varchar(500),
+   From varchar(50) NOT NULL,
+)
+
