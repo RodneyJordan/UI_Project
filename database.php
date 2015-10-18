7d7c6ebed4d49a4bed9f0b80dc78a975241c8e07
Rodney Jordan <rodneycjordan@gmail.com>
2015-10-17 23:08:22 -0500

    added a database

---
+++ database.php
@@ -0,0 +1,18 @@
+<?php
+require_once 'login.php';
+$db_server = mysqli_connect($db_hostname, $db_username, $db_password);
+if(!$db_server) die("Unable to connect to MySQL: " .mysql_error());
+mysqli_select_db($db_server, $db_database)
+   or die("Unable to select database: " .mysql_error());
+
+if(isset($_POST['email']) && isset($_POST['password'])) {
+   $email = $_POST['email'];
+   $password = $_POST['password'];
+
+   $query = "INSERT INTO USER(UserName, Password) VALUES('$email', '$password')";
+   $result = mysqli_query($db_server, $query);
+   if(!$result) die ("Database access failed: " .mysql_error());
+   echo "New record created successfully.";
+}
+
+?>
--- osc_database.sql
+++ osc_database.sql
@@ -1,17 +1,21 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
 CREATE TABLE IF NOT EXISTS USER (
-   UID int(8) NOT NULL, AUTO_INCREMENT,
+   UID int(8) NOT NULL AUTO_INCREMENT,
    Fname varchar(15),
    Minit char(1),
    Lname varchar(15),
    UserName varchar(50) NOT NULL UNIQUE,
    Password varchar(15) NOT NULL UNIQUE,
+   Avatar  varchar(50),
    PRIMARY KEY (UID),
 ) ENGINE = MyISAM;
 
-CREATE TABLE IF NOT EXISTS MESSAGE (
+/*CREATE TABLE IF NOT EXISTS MESSAGE (
    
    UID int(8) NOT NULL,
    Msg varchar(500),
    From varchar(50) NOT NULL,
-)
+)*/
 
